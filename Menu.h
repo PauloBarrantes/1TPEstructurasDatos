2f95172f10594b5bef9c30533cf6408a557da173
Paulo César Barrantes Aguilar <paulobarrante@gmail.com>
2017-10-05 02:27:54 -0600

    Menu
    Arbol y Cola

--- Menu.h
+++ Menu.h
@@ -15,7 +15,35 @@ class Menu {
         void instruccionesArbol();
         void instruccionesCola();
 
-
+        //Arbol
+        void iniciar();
+        void destruir();
+        void vaciar();
+        void vacia();
+        void vaciar();
+        void raiz();
+        void hijoMasIzq();
+        void hermanoDer();
+        void padre();
+        void esHoja();
+        void etiqueta();
+        void numNodos();
+        void numHijos();
+        void modificarEtiq();
+        void agregarHijoIesimo();
+        void borrarHoja();
+        void ponerRaiz();
+        void imprimir();
+        //Cola
+        void iniciar();
+        void destruir();
+        void vaciar();
+        void vacia();
+        void encolar();
+        void desencolar();
+        void numElementos();
+        void frente();
+        void ultimo();
 };
 // g++ main.cpp cola.cpp arbolsalpadre.cpp menu.cpp -o ejecutable
 
--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include "Cola.h"
-#include "ArbolSalPadre.h"
-#include <string>
 #include "Menu.h"
 using namespace std;
 
 int main(){
-
-
-    string palabra;
-    cin >> palabra;
-    cout << palabra <<endl;
-    if(palabra == "Fa"){
-        cout << "Comparar funciona" <<endl;
-    }
-
     Menu menu;
-    
-
-
 }
