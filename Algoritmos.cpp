3ee3202e35af24ee429a998d6dfeaaa9b550cade
Paulo César Barrantes Aguilar <paulobarrante@gmail.com>
2017-10-08 22:22:40 -0600

    Clase Algoritmos

---
+++ Algoritmos.cpp
@@ -0,0 +1,30 @@
+#include "Algoritmos.h"
+#include <iostream>
+#include "Cola.h"
+using namespace std;
+
+typedef Arbol::Nodo NodoArbol;
+
+Arbol* Algoritmos::copiarArbol(Arbol* arbol1){
+    Arbol* arbol2 = new Arbol(); //Iniciamos la copia del árbol 1
+    if(!arbol1->vacia()){
+
+
+    }
+
+
+
+    return arbol2
+}
+void Algoritmos::listarEtiquetasDeNodo(NodoArbol nodo){
+
+}
+void Algoritmos::listarEtiquetasiesimoNivel(int nivel){
+
+}
+void Algoritmos::borrarSubArbol(NodoArbol nodo){
+
+}
+int Algoritmos::iguales(Arbol* arbol1, Arbol* arbol2){
+
+}
---
+++ Algoritmos.h
@@ -0,0 +1,16 @@
+#ifndef Algoritmos_2017
+#define Algoritmos_2017
+#include "ArbolSalPadre.h"
+class Algoritmos{
+    private:
+
+    public:
+        Arbol copiarArbol(Arbol)
+        void listarEtiquetasDeNodo(Nodo);
+        void listarEtiquetasiesimoNivel(int);
+        void borrarSubArbol(Nodo);
+        int iguales(Arbol, Arbol);
+
+};
+
+#endif
