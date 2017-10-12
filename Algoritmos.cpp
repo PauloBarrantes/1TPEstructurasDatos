cba6ab0c58c159ad8c38d418fa06ce6cc5d42b91
Paulo César Barrantes Aguilar <paulobarrante@gmail.com>
2017-10-11 20:56:30 -0600

    Recorcholis

--- Algoritmos.cpp
+++ Algoritmos.cpp
@@ -10,9 +10,6 @@ Algoritmos::Algoritmos(){
 }
 Algoritmos::~Algoritmos(){
 }
-
-Algoritmos::
-
 Arbol* Algoritmos::copiarArbol(Arbol* arbol1){
     Arbol* arbol2 = new Arbol(); //Iniciamos la copia del árbol 1
     if(!arbol1->vacia()){
@@ -58,7 +55,6 @@ int Algoritmos::iguales(Arbol* arbol1, Arbol* arbol2){
             Cola<Arbol::Nodo> cola1;
             Cola<Arbol::Nodo> cola2;
             cola1.encolar(arbol1->raiz());
-            arbol2->ponerRaiz(arbol1->etiqueta(arbol1->raiz()));
             cola2.encolar(arbol2->raiz());
             while (!cola1.vacia() && !iguales) {
                 Arbol::Nodo nodo1 = cola1.desencolar();
@@ -67,11 +63,13 @@ int Algoritmos::iguales(Arbol* arbol1, Arbol* arbol2){
                 Arbol::Nodo nodoh2 = arbol2->hijoMasIzq(nodo1);
                 while(nodoh1 != nodoNulo && !iguales){
                     if(nodoh1 == nodoh2){
-
+                        cola1.encolar(nodoh1);
+                        nodoh1 = arbol1->hermanoDer(nodoh1);
+                        cola2.encolar(nodoh2);
+                        nodoh2 = arbol->hermanoDer(nodo2);
+                    }else{
+                        iguales
                     }
-                    cola1.encolar(nodoh1);
-                    nodoh1 = arbol1->hermanoDer(nodoh1);
-                    ++contador;
                 }
             }
         }
