4405bd8cb2591b3cc542910bc4c75fab5ad9e884
Paulo César Barrantes Aguilar <paulobarrante@gmail.com>
2017-10-17 14:45:23 -0600

    Documentación

--- Algoritmos.h
+++ Algoritmos.h
@@ -1,3 +1,9 @@
+/*!
+   \file "Algoritmos.h"
+   \brief "Clase que contiene los algoritmos para el modelo árbol"
+   \author "Paulo B - Fabian A - André F"
+   \date "17"/"10"/"2017"
+*/
 #ifndef Algoritmos_2017
 #define Algoritmos_2017
 #include "ArbolSalPadre.h"
@@ -13,12 +19,42 @@ class Algoritmos{
 		int averiguarNivelsPreOrden(Arbol*);
 		void averiguarNivelesPreOrdenR(Arbol*,Arbol::Nodo,int,int&);
 		int profundidadNodo(Arbol*,Arbol::Nodo);
-        Arbol* copiarArbol(Arbol*);
-        void listarEtiquetasDeNodo(Arbol*,Arbol::Nodo);
-        void listarEtiquetas_iesimoNivel(Arbol*, int );
-        void listarEtiquetas_iesimoNivelRec(Arbol*, Arbol::Nodo, int,int);
 
+        /*!
+           \brief "Description"
+           \param "Param description"s
+           \return "Return of the function"
+        */
+        Arbol* copiarArbol(Arbol*);
+        /*!
+           \brief "Description"
+           \param "Param description"s
+           \return "Return of the function"
+        */
+        void listarEtiquetasDeNodo(Arbol*,Arbol::Nodo);
+        /*!
+           \brief "Description"
+           \param "Param description"s
+           \return "Return of the function"
+        */
+        void listarEtiquetas_iesimoNivel(Arbol*, int );
+        /*!
+           \brief "Description"
+           \param "Param description"s
+           \return "Return of the function"
+        */
+        void listarEtiquetas_iesimoNivelRec(Arbol*, Arbol::Nodo, int,int);
+        /*!
+           \brief "Description"
+           \param "Param description"s
+           \return "Return of the function"
+        */
         void borrarSubArbol(Arbol::Nodo);
+        /*!
+           \brief "Description"
+           \param "Param description"s
+           \return "Return of the function"
+        */
         int iguales(Arbol*, Arbol*);
 
 
