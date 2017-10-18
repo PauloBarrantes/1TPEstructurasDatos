9d5db4d4ed19ddd451160f3e3164fd83c03e99b0
Paulo César Barrantes Aguilar <paulobarrante@gmail.com>
2017-10-18 12:10:02 -0600

    Algoritmos arreglado mas o menos

--- Algoritmos.h
+++ Algoritmos.h
@@ -15,38 +15,27 @@ class Algoritmos{
     public:
         Algoritmos();
         ~Algoritmos();
-		#ifndef Algoritmos_2017
-#define Algoritmos_2017
-#include "ArbolSalPadre.h"
-class Algoritmos{
-    private:
-        int nodoNulo;
-        Arbol* arbol1;
-    public:
-        Algoritmos();
-        ~Algoritmos();
-		
 		/*!
 			\brief "Verifica si el árbol tiene etiquetas repetidos"
 			\param "Entra el Árbol en el cual se buscaran etiquetas repetidas"
 			\return "1 sí el árbol tiene repetidos"
 		*/
 		int hayRepetidos(Arbol*);
-		
+
 		/*!
 			\brief "Averigua la cantidad de niveles que tiene el Árbol, en recorrido por niveles"
 			\param "Entra el Árbol al que le vamos a contar los niveles"
 			\return "Número de niveles que tiene el Árbol"
 		*/
 		int averiguarNivelesEnRecorridoPorNiveles(Arbol*);
-		
+
 		/*!
 			\brief "Averigua el número de niveles del Árbol, en pre orden"
 			\param "Entra el Árbol al que le vamos a contar los niveles"
 			\return "Número de niveles que tiene el Árbol"
 		*/
 		int averiguarNivelsPreOrden(Arbol*);
-		
+
 		/*!
 			\brief "Crea un nodo, pero sin hijos ni hermanos"
 			\param "Entra el Árbol al que le vamos a contar los niveles"
@@ -56,7 +45,7 @@ class Algoritmos{
 			\return "Número de niveles que tiene el árbol"
 		*/
 		void averiguarNivelesPreOrdenR(Arbol*,Arbol::Nodo,int,int&);
-		
+
 		/*!
 			\brief "Dice  que profundidad está el nodo"
 			\param "Entra el Árbol donde se buscara el nivel del nodo"
@@ -64,14 +53,14 @@ class Algoritmos{
 			\return "Nivel en el que se encuentra el nodo"
 		*/
 		int profundidadNodo(Arbol*,Arbol::Nodo);
-		
+
 		/*!
 			\brief "Realiza una copia del Árbol resivido como parámetro"
 			\param "Entra el Árbol que va a ser copiado"
 			\return "Retorna una copia del Árbol"
 		*/
         Arbol* copiarArbol(Arbol*);
-		
+
 		/*!
 			\brief "Lista las etiquetas de los hijos del Nodo"
 			\param "Entra el Árbol donde se encuentra el nodo recibido como parámetro"
@@ -79,7 +68,7 @@ class Algoritmos{
 			\return "No retorna nada"
 		*/
         void listarEtiquetasDeNodo(Arbol*,Arbol::Nodo);
-		
+
 		/*!
 			\brief "Lista las etiquetas del Árbol que se encuentran en el iésimo nivel"
 			\param "Entra el Árbol al que le vamos a listar las etiquetas del iésimo nivel"
@@ -87,7 +76,7 @@ class Algoritmos{
 			\return "No retorna nada"
 		*/
         void listarEtiquetas_iesimoNivel(Arbol*, int );
-		
+
 		/*!
 			\brief "Lista las etiquetas del Árbol que se encuentran en el iésimo nivel"
 			\param "Entra el Árbol al que le vamos a listar las etiquetas del iésimo nivel"
@@ -105,7 +94,7 @@ class Algoritmos{
 			\return "No retorna nada"
 		*/
         void borrarSubArbol(Arbol*, Arbol::Nodo);
-		
+
 		/*!
 			\brief "Dice sí los Árboles que se reciben como parámetros son iguales"
 			\param "Entra el primero de los Árbles que se va a comparar"
