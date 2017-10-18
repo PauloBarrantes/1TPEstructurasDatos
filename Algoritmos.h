33943d41a907f83d93211ae5e716aa99cf2183c4
Ducan-Ally <Fakebian98@gmail.com>
2017-10-17 20:01:28 -0600

    Documentación de los Álgoritmos

--- Algoritmos.h
+++ Algoritmos.h
@@ -15,39 +15,110 @@ class Algoritmos{
     public:
         Algoritmos();
         ~Algoritmos();
+		#ifndef Algoritmos_2017
+#define Algoritmos_2017
+#include "ArbolSalPadre.h"
+class Algoritmos{
+    private:
+        int nodoNulo;
+        Arbol* arbol1;
+    public:
+        Algoritmos();
+        ~Algoritmos();
+		
+		/*!
+			\brief "Verifica si el árbol tiene etiquetas repetidos"
+			\param "Entra el Árbol en el cual se buscaran etiquetas repetidas"
+			\return "1 sí el árbol tiene repetidos"
+		*/
 		int hayRepetidos(Arbol*);
+		
+		/*!
+			\brief "Averigua la cantidad de niveles que tiene el Árbol, en recorrido por niveles"
+			\param "Entra el Árbol al que le vamos a contar los niveles"
+			\return "Número de niveles que tiene el Árbol"
+		*/
 		int averiguarNivelesEnRecorridoPorNiveles(Arbol*);
+		
+		/*!
+			\brief "Averigua el número de niveles del Árbol, en pre orden"
+			\param "Entra el Árbol al que le vamos a contar los niveles"
+			\return "Número de niveles que tiene el Árbol"
+		*/
 		int averiguarNivelsPreOrden(Arbol*);
+		
+		/*!
+			\brief "Crea un nodo, pero sin hijos ni hermanos"
+			\param "Entra el Árbol al que le vamos a contar los niveles"
+			\param "Entra el Nodo actual, sobre el cual vamos recorriendo el Árbol"
+			\param "Entra el nivel actual, por el que vamos en el Árbol"
+			\param "Entra el nivel máximo que lleva el Árbol"
+			\return "Número de niveles que tiene el árbol"
+		*/
 		void averiguarNivelesPreOrdenR(Arbol*,Arbol::Nodo,int,int&);
+		
+		/*!
+			\brief "Dice  que profundidad está el nodo"
+			\param "Entra el Árbol donde se buscara el nivel del nodo"
+			\param "Entra el nodo al que se le va a averiguar el nivel"
+			\return "Nivel en el que se encuentra el nodo"
+		*/
 		int profundidadNodo(Arbol*,Arbol::Nodo);
-
-        /*!
-           \brief "Description"
-           \param "Param description"s
-           \return "Return of the function"
-        */
+		
+		/*!
+			\brief "Realiza una copia del Árbol resivido como parámetro"
+			\param "Entra el Árbol que va a ser copiado"
+			\return "Retorna una copia del Árbol"
+		*/
         Arbol* copiarArbol(Arbol*);
-        /*!
-           \brief "Description"
-           \param "Param description"s
-           \return "Return of the function"
-        */
+		
+		/*!
+			\brief "Lista las etiquetas de los hijos del Nodo"
+			\param "Entra el Árbol donde se encuentra el nodo recibido como parámetro"
+			\param "Nodo al que le vamos a listar las etiquetas de sus hijos"
+			\return "No retorna nada"
+		*/
         void listarEtiquetasDeNodo(Arbol*,Arbol::Nodo);
-        /*!
-           \brief "Description"
-           \param "Param description"s
-           \return "Return of the function"
-        */
+		
+		/*!
+			\brief "Lista las etiquetas del Árbol que se encuentran en el iésimo nivel"
+			\param "Entra el Árbol al que le vamos a listar las etiquetas del iésimo nivel"
+			\param "Entra el nivel del cual se quieren listar las etiquetas"
+			\return "No retorna nada"
+		*/
         void listarEtiquetas_iesimoNivel(Arbol*, int );
-        /*!
-           \brief "Description"
-           \param "Param description"s
-           \return "Return of the function"
-        */
+		
+		/*!
+			\brief "Lista las etiquetas del Árbol que se encuentran en el iésimo nivel"
+			\param "Entra el Árbol al que le vamos a listar las etiquetas del iésimo nivel"
+			\param "Entra el Nodo actual por el que va el recorrido del Árbol"
+			\param "Entra el nivel actual por el que va en el Árbol"
+			\param "Entra el nivel del cual se quieren listar las etiquetas"
+			\return "No retorna nada"
+		*/
         void listarEtiquetas_iesimoNivelRec(Arbol*, Arbol::Nodo, int,int);
 
+		/*!
+			\brief "Borra el su Árbol, que se forma apartir dl nodo que se recibe como parámetro"
+			\param "Entra el Árbol al que le vamos a borrar el Sub Árbol"
+			\param "Entra el Nodo donde comienza el sub Árbol a borrar"
+			\return "No retorna nada"
+		*/
         void borrarSubArbol(Arbol*, Arbol::Nodo);
+		
+		/*!
+			\brief "Dice sí los Árboles que se reciben como parámetros son iguales"
+			\param "Entra el primero de los Árbles que se va a comparar"
+			\param "Entra el segundo de los Árboles que se va a comparar"
+			\return "N1 si los Árboles son iguales"
+		*/
         int iguales(Arbol*, Arbol*);
+
+
+};
+
+#endif
+
 
 
 };
